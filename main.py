c93b2c7b8bd1fa93e0c65b0fae80f02f6e657cfe
Ujma Kacper <276010@student.pwr.edu.pl>
2026-03-09 14:11:06 +0100

    almost nothing

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-import tkinter
+import tkinter as tk
+from tkinter import *
 
-print("a")
+
+def shut_down():
+    root.destroy()
+
+root = Tk()
+root.title("Apka")
+
+mainframe = tk.Frame(root)
+
+
+
+loop_end = True
+counter = 0
+while root.winfo_exists():
+    shut = str(input("Write shut if you want to close app\n"))
+    shut = shut.lower()
+    shut = shut.replace(" ","")
+    if shut == "shut":
+        shut_down()
+        break
+
+root.mainloop()
